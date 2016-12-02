e0e42e6aed81f38a6de0cac7af2e205ecdac4262
antoine_galataud <antoine@foobot.io>
2016-12-02 10:50:11 +0100

    use http for git repo clone

--- spark_ec2.py
+++ spark_ec2.py
@@ -105,10 +105,10 @@ SPARK_TACHYON_MAP = {
 }
 
 DEFAULT_SPARK_VERSION = SPARK_EC2_VERSION
-DEFAULT_SPARK_GITHUB_REPO = "https://github.com/apache/spark"
+DEFAULT_SPARK_GITHUB_REPO = "http://github.com/apache/spark"
 
 # Default location to get the spark-ec2 scripts (and ami-list) from
-DEFAULT_SPARK_EC2_GITHUB_REPO = "https://github.com/antoine-galataud/spark-ec2"
+DEFAULT_SPARK_EC2_GITHUB_REPO = "http://github.com/antoine-galataud/spark-ec2"
 DEFAULT_SPARK_EC2_BRANCH = "branch-2.0-china"
 
 
